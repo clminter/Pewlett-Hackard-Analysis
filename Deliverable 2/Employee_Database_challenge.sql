de2fe3b06561ed4f691b77cb88662e46cbd223e5
clminter <81878169+clminter@users.noreply.github.com>
2021-06-19 17:47:42 -0500

    Add files via upload

---
+++ Deliverable 2/Employee_Database_challenge.sql
@@ -0,0 +1,66 @@
+
+
+-- Create new table for  number of retiring employees by retirement titles.
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+--INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;	
+	
+SELECT * FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT  
+	DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+--INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+SELECT * FROM unique_titles;
+
+SELECT COUNT(ut.title), ut.title
+--INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY count DESC;
+
+SELECT * FROM retiring_titles;
+
+-- Create Mentorship Eligibility table
+SELECT  
+	DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)	
+INNER JOIN titles as ti
+ON (ti.emp_no = de.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	ORDER BY e.emp_no ASC;
+
+SELECT * FROM mentorship_eligibility;
+
+
+
+
+
+
+
+
